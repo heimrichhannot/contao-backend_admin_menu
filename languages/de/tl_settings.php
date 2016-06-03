26a64ec0c4656c0cfde6e303bcc5b72516d9ff46
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-06-03 17:08:09 +0200

    fixed setting locales

---
+++ languages/de/tl_settings.php
@@ -0,0 +1,13 @@
+<?php
+
+$arrLang = &$GLOBALS['TL_LANG']['tl_settings'];
+
+/**
+ * Fields
+ */
+
+$arrLang['backendAdminMenuActiveActions'] = array('Aktionen', 'Wählen Sie hier die gewünschten Aktionen aus');
+/**
+ * Legends
+ */
+$arrLang['backend_admin_menu_legend'] = 'Backend Admin Menu';
